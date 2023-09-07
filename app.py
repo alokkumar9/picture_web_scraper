26b3fdd6693a0815685dba0911476c9a4def353a
alok <alokkumar991992@gmail.com>
2023-09-08 00:29:33 +0530

    image loading

--- app.py
+++ app.py
@@ -9,25 +9,29 @@ st.markdown("<h1 style='text-align:center;'> Image Web Scraper</h1>", unsafe_all
 with st.form("Search"):
     keyword=st.text_input("Enter image keyword you want to search")
     search=st.form_submit_button("Search")
-    placeholder=st.empty()
+
+
+placeholder=st.empty() 
 
 if keyword:
     page=requests.get(f"https://unsplash.com/s/photos/{keyword}")
     soup=BeautifulSoup(page.content,"html.parser")
-    rows=soup.find_all("div",class_="ripi6")
+    rows=soup.find_all("div",class_="MorZF")
+    
     col1,col2=placeholder.columns(2)
     for index,row in enumerate(rows):
-        figures=row.find_all("figure")
-        for i in range(2):
-            img=figures[i].find("img", class_="YVj9w")
-            list=img['srcset'].split("?")
-            anchor=figures[i].find("a", class_="rEAWd")
-            if i==0:
-                col1.image(list[0])
-                btn=col1.button("Download", key=str(index)+str(i))
-                if btn:
-                    webbrowser.open_new_tab("http://unsplash.com/download"+anchor["href"])
-            time.sleep(0.5)
+         img=row.find("img")
+         imgsrc=img["src"]
+         print(imgsrc)
+         if index%2==0:
+            col1.image(imgsrc)
+             btn=col1.button("Download", key=str(index))
+         else:
+            col2.image(imgsrc)
+            btn=col1.button("Download", key=str(index))
+         
+        
+        
 
         
 
